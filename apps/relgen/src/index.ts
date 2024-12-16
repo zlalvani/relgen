e032ec98eaf3e68b11ae193b151a81664dd2e4be
zlalvani <zlalvani@gmail.com>
2024-12-16 03:09:46 -0500

    add vercel anthropic sdk

--- apps/relgen/src/index.ts
+++ apps/relgen/src/index.ts
@@ -2,4 +2,8 @@ import { createRelgen } from 'relgen-core';
 
 const relgen = createRelgen({
   llmApiKey: 'my-api-key',
+  model: {
+    provider: 'openai',
+    modelId: 'gpt-4o-mini',
+  },
 });
--- packages/relgen-core/src/index.ts
+++ packages/relgen-core/src/index.ts
@@ -1,14 +1,28 @@
+import { createAnthropic } from '@ai-sdk/anthropic';
+import type { AnthropicMessagesModelId } from '@ai-sdk/anthropic/internal';
 import { createOpenAI } from '@ai-sdk/openai';
+import type { OpenAIChatModelId } from '@ai-sdk/openai/internal';
 import { type LanguageModel, generateText } from 'ai';
 
 export type RelgenOptions = {
   llmApiKey: string;
+  model:
+    | {
+        provider: 'openai';
+        modelId: OpenAIChatModelId;
+      }
+    | {
+        provider: 'anthropic';
+        modelId: AnthropicMessagesModelId;
+      };
+  template?: string;
 };
 
 const relgen = ({
   model,
 }: {
   model: LanguageModel;
+  template?: string;
 }) => {
   return {
     generate: async () => {
@@ -20,13 +34,30 @@ const relgen = ({
   };
 };
 
-export const createRelgen = ({ llmApiKey }: RelgenOptions) => {
-  const model = createOpenAI({ apiKey: llmApiKey }).chat(
-    'gpt-4o-mini-2024-07-18'
-  );
+export const createRelgen = (options: RelgenOptions) => {
+  const { llmApiKey, template } = options;
+
+  let model: LanguageModel;
+
+  switch (options.model.provider) {
+    case 'openai': {
+      model = createOpenAI({ apiKey: llmApiKey }).chat(options.model.modelId);
+      break;
+    }
+    case 'anthropic': {
+      model = createAnthropic({ apiKey: llmApiKey }).languageModel(
+        options.model.modelId
+      );
+      break;
+    }
+    default: {
+      throw new Error('Invalid model provider');
+    }
+  }
 
   return relgen({
     model,
+    template,
   });
 };
 
